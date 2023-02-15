3a3950ba7603282bcad1c14caf59b260922c54a3
gcarolina <karolinaganizoda@gmail.com>
2023-02-16 00:43:53 +0300

    Project created

---
+++ ColorPickerHW/ColorPickerHW/ColorPickerHWApp.swift
@@ -0,0 +1,17 @@
+//
+//  ColorPickerHWApp.swift
+//  ColorPickerHW
+//
+//  Created by Carolina on 16.02.23.
+//
+
+import SwiftUI
+
+@main
+struct ColorPickerHWApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
